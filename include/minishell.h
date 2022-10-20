89b7236fb98269cd3c568c325c7d8c6882cb801c
Henrique <hpcavalcante@gmail.com>
2022-10-20 11:23:54 -0300

    include

---
+++ include/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepiment <hepiment@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 11:15:48 by hepiment          #+#    #+#             */
+/*   Updated: 2022/10/20 11:22:08 by hepiment         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_SHELL
+# define MINI_SHELL
+
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/types.h>
+# include </usr/include/readline/readline.h>
+# include <../libft/libft.h>
+
+# define clear() printf("\033[H\033[J")
+
+#endif
