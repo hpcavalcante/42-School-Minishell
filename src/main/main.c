3bd915da74df95244df7ef1f132f6317bcefed14
root <root@DESKTOP-JAODQIH.localdomain>
2022-10-20 11:51:57 -0300

    add_error

--- src/main/main.c
+++ src/main/main.c
@@ -6,37 +6,37 @@
 /*   By: gissao-m <gissao-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 14:30:49 by gissao-m          #+#    #+#             */
-/*   Updated: 2022/10/19 17:17:18 by gissao-m         ###   ########.fr       */
+/*   Updated: 2022/10/20 11:24:55 by gissao-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <readline/readline.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <sys/types.h>
-#include </usr/include/readline/readline.h>
+#include "minishell.h"
 
-// void	fazer_prompt(int signal)
-// {
-// 	(void) signal;
-// 	printf("HG\n");
-// 	rl_replace_line("", 0);
-// 	rl_on_new_line();
-// 	rl_redisplay();
-// }
+void	fazer_prompt(int signal)
+{
+	(void) signal;
+	printf("HG\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
 
-// int main(int argc,char **argv, char **envp)
-// {
-// 	signal(SIGINT, fazer_prompt);
-// 	return (0);
-// }
+int main(int argc,char **argv, char **envp)
+{
+	if (argc != 1)
+	{
+		perror("Error");
+		exit(1);
+		//termino do programa com algum erro.
+	}
+	
+	signal(SIGINT, fazer_prompt);
+	return (0);
+}
 
 
 int main()
 {
-    printf( "%s\n", readline( "test> " ) );
-    return 0;
+	printf( "%s\n", readline( "test> " ) );
+	return 0;
 }
